1a3d74e7c22e98c81f4746f0ec53a3b031aef90f
Aleksandr Cheverda <alexar@ua.fm>
2022-11-21 19:46:50 +0200

    bag fix ContactForm

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -49,7 +49,7 @@ const ContactForm = ({ onSubmit }) => {
       <Input
         type="tel"
         name="number"
-        value={this.state.number}
+        value={number}
         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
         required
